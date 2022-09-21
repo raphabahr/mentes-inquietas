4741b786186336eb7f861be8cf77397b5c3cb1e7
Raphael Bahr <raphabahr@gmail.com>
2022-09-21 18:39:40 -0300

    update light on/off

--- js/index.js
+++ js/index.js
@@ -16,15 +16,3 @@ if (botaoMenu) {
     });
 }
 
-
-// botao light on off
-
-const checkbox = document.getElementById('chk');
-
-checkbox.addEventListener('change', () => {
-  document.body.classList.toggle('dark');
-});
-
-checkbox.addEventListener('change', () => {
-  document.querySelector.classList.toggle('dark');
-});
---
+++ js/light.js
@@ -0,0 +1,12 @@
+// botao light on off
+
+const checkbox = document.getElementById('chk');
+
+checkbox.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  document.section.classList.toggle('dark');
+});
+
+checkbox.addEventListener('change', () => {
+  document.querySelector.classList.toggle('dark');
+});
